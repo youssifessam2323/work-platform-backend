d24e9831f72f666bcbf9cb526679a4dcf13598ae
youssifessam2323 <essamyoussif3@gmail.com>
2020-12-29 01:59:53 +0200

    first commit

--- Dtos/LoginDto.cs
+++ Dtos/LoginDto.cs
@@ -1,13 +1,14 @@
 using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Net.Sockets;
+using work_platform_backend.Validations;
 
 namespace work_platform_backend.Dtos
 {
     public class LoginDto
     {
 
-       
+        [EmailAttributeUniqueValidation] // not tested yet
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
